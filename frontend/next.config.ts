109ff17947a8bc1bf2ce9c00ee47a290eff4a643
AsepNurdinDev <asep@student.undiksha.ac.id>
2026-05-28 16:53:30 +0800

    mengubah config next js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,22 @@ const nextConfig = {
     API_URL_SERVER: process.env.API_URL_SERVER,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.asepblog.my.id',
+        pathname: '/uploads/**',
+      },
+      // Cadangan jika Anda testing di localhost agar gambar tetap muncul
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
